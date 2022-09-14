78e01c6cb3584c05b700829b49000850c139910c
ST10117355 <ST10117355@vcconnect.edu.za>
2022-09-14 11:18:16 +0200

    new

--- app/src/main/java/com/varsitycollege/simpleweather/MainActivity.java
+++ app/src/main/java/com/varsitycollege/simpleweather/MainActivity.java
@@ -1,14 +1,107 @@
 package com.varsitycollege.simpleweather;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText edtUsername;
+    EditText edtPassword;
+    String email, password;
+    Button btnLogin, btnSign;
+    final int REQ_CODE = 100;
+    private String mCustomToken = "LOGIN FEEDBACK";
+    private FirebaseAuth mAuth;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edtUsername = findViewById(R.id.edtUsername);
+        edtPassword = findViewById(R.id.edtPassword);
+        btnSign = findViewById(R.id.btnSign);
+        btnLogin = findViewById(R.id.btnLogin);
+        mAuth = FirebaseAuth.getInstance();
+
+        btnSign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String [] permissions = {Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.INTERNET};
+                if(!AppPermissions.hasPermissions(MainActivity.this, permissions)){
+                    ActivityCompat.requestPermissions(MainActivity.this, permissions, REQ_CODE);
+                }
+
+                email = edtUsername.getText().toString().trim();
+                password = edtPassword.getText().toString().trim();
+
+                mAuth.createUserWithEmailAndPassword(email,password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>(){
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task){
+                                if(task.isSuccessful()){
+                                    Toast.makeText(MainActivity.this,"user created" + mAuth.getCurrentUser().getEmail(), Toast.LENGTH_SHORT).show();
+
+                                }
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+
+
+                    }
+                });
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                email = edtUsername.getText().toString().trim();
+                password = edtPassword.getText().toString().trim();
+                mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(MainActivity.this,"user"
+                                    + mAuth.getCurrentUser().getEmail(), Toast.LENGTH_SHORT).show();
+                            //Intent i = new Intent(MainActivity.this,);
+                            //startActivity(i);
+                        }
+
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+
+            }
+        });
+
+
+
+
     }
 }
